ff6f7bcff78ad7e63ca02fd03d41ad27572f13de
Julian Luhring <luhring.julian@gmail.com>
2023-08-05 14:53:34 -0300

    update files

--- README.md
+++ README.md
@@ -1,15 +1,3 @@
-Meu primeiro projeto em JavaScript
-
-Clone - Calculadora iOS
-
-Todos os números, funções e operações estão funcionais
-
-Pode ter alguns problemas, mas eu não consegui resolver (ainda).
-
-2023-08-03 - Atualização de código para otimização
-
-===========================================================
-
 My first JS project
 
 iOS Calculator clone
@@ -18,4 +6,5 @@ All numbers and operators are functional.
 
 Could have some issues, but I don't figure out how to solve them (yet).
 
-2023-08-03 - Code update optimization
+2023-08-03 - Code optimization update
+2023-08-05 - Code identation update
--- script.js
+++ script.js
@@ -7,186 +7,158 @@ let valorDisplay = '0';
 let operacao = '';
 let valorAnterior = 0;
 
-
 //Funções
 //
 //
 //Atualizar o Display
 function atualizaDisplay() {
-  const display = document.getElementById('display');
-  display.textContent = valorDisplay;
+	const display = document.getElementById('display');
+	display.textContent = valorDisplay;
 }
 
 //Inserir os números
 function inserirNumero(num) {
-  if(valorDisplay === '0') {
-    valorDisplay = num;
-  }else {
-    valorDisplay += num;
-  }
-  atualizaDisplay();
+	if (valorDisplay === '0') {
+		valorDisplay = num;
+	} else {
+		valorDisplay += num;
+	}
+	atualizaDisplay();
 }
 
 //Limpar o Display
 function limpaDisplay() {
-  valorDisplay = '0';
-  operacao = '';
-  valorAnterior = 0;
-  atualizaDisplay();
+	valorDisplay = '0';
+	operacao = '';
+	valorAnterior = 0;
+	atualizaDisplay();
 }
 
 //Mudança de Sinal
 function mudaSinal() {
-  valorDisplay = valorDisplay * -1;
-  atualizaDisplay();
+	valorDisplay = valorDisplay * -1;
+	atualizaDisplay();
 }
 
 //Calculo de Porcentagem
 function porcent() {
-  valorDisplay = valorDisplay / 100;
-  atualizaDisplay();
+	valorDisplay = valorDisplay / 100;
+	atualizaDisplay();
 }
 
 //Verifica se existe um operador
 function operador(op) {
-  if(operacao != '') {
-    resultado();
-  }
-  operacao = op;
-  valorAnterior = parseFloat(valorDisplay);
-  valorDisplay = '0';
-  atualizaDisplay();
+	if (operacao != '') {
+		resultado();
+	}
+	operacao = op;
+	valorAnterior = parseFloat(valorDisplay);
+	valorDisplay = '0';
+	atualizaDisplay();
 }
 
 //Cálculo do Resultado
 function resultado() {
-  const valorAtual = parseFloat(valorDisplay);
-  switch (operacao) {
-    case '/':
-      valorDisplay = (valorAnterior / valorAtual).toString();
-      break;
+	const valorAtual = parseFloat(valorDisplay);
+	switch (operacao) {
+		case '/':
+			valorDisplay = (valorAnterior / valorAtual).toString();
+			break;
 
-    case 'x':
-      valorDisplay = (valorAnterior * valorAtual).toString();
-      break;
+		case 'x':
+			valorDisplay = (valorAnterior * valorAtual).toString();
+			break;
 
-    case '-':
-      valorDisplay = (valorAnterior - valorAtual).toString();
-      break;
-      
-    case '+':
-      valorDisplay = (valorAnterior + valorAtual).toString();
-      break;
-  
-    default:
-      break;
-  }
-  operacao = '';
-  atualizaDisplay();
+		case '-':
+			valorDisplay = (valorAnterior - valorAtual).toString();
+			break;
+
+		case '+':
+			valorDisplay = (valorAnterior + valorAtual).toString();
+			break;
+
+		default:
+			break;
+	}
+	operacao = '';
+	atualizaDisplay();
 }
 
 //Chamando Função para atualizar tela
 atualizaDisplay();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 let displayValue = '0';
 let operator = '';
 let prevValue = 0;
 
-function displayUpdate(){
-  const display = document.getElementById('display');
-  display.textContent = displayValue;
+function displayUpdate() {
+	const display = document.getElementById('display');
+	display.textContent = displayValue;
 }
 
-function clearDisplay(){
-  displayValue = '0';
-  operator = '';
-  prevValue = 0;
+function clearDisplay() {
+	displayValue = '0';
+	operator = '';
+	prevValue = 0;
 
-  displayUpdate();
+	displayUpdate();
 }
 
-function appendOperator(op){
-  if(operator !== ''){
-    calculateResult()
-  }
-  operator = op;
-  prevValue = parseFloat(displayValue);
-  
-  displayValue = '0';
+function appendOperator(op) {
+	if (operator !== '') {
+		calculateResult();
+	}
+	operator = op;
+	prevValue = parseFloat(displayValue);
 
-  displayUpdate();
+	displayValue = '0';
+
+	displayUpdate();
 }
 
-function changeSignal(){
-  displayValue = displayValue * -1;
-  displayUpdate();
+function changeSignal() {
+	displayValue = displayValue * -1;
+	displayUpdate();
 }
 
-function percentage(){
-  displayValue = displayValue / 100;
-  displayUpdate();
-};
-
-function calculateResult(){
-  const currentValue = parseFloat(displayValue);
-  switch(operator){
-    case '+/-':
-      displayValue = (prevValue * -1).toString();
-      displayUpdate();
-      break;
-    case '/':
-      displayValue = (prevValue / currentValue).toString();
-      break;
-    case 'x':
-      displayValue = (prevValue * currentValue).toString();
-      break;
-    case '-':
-      displayValue = (prevValue - currentValue).toString();
-      break;
-    case '+':
-      displayValue = (prevValue + currentValue).toString();
-      break;
-  }
-  operator = '';
-
-  displayUpdate();
+function percentage() {
+	displayValue = displayValue / 100;
+	displayUpdate();
 }
 
-function appendNumber(num){
-  if(displayValue === '0'){
-    displayValue = num;
-  }else{
-    displayValue += num;
-  }
+function calculateResult() {
+	const currentValue = parseFloat(displayValue);
+	switch (operator) {
+		case '+/-':
+			displayValue = (prevValue * -1).toString();
+			displayUpdate();
+			break;
+		case '/':
+			displayValue = (prevValue / currentValue).toString();
+			break;
+		case 'x':
+			displayValue = (prevValue * currentValue).toString();
+			break;
+		case '-':
+			displayValue = (prevValue - currentValue).toString();
+			break;
+		case '+':
+			displayValue = (prevValue + currentValue).toString();
+			break;
+	}
+	operator = '';
 
-  displayUpdate();
+	displayUpdate();
+}
+
+function appendNumber(num) {
+	if (displayValue === '0') {
+		displayValue = num;
+	} else {
+		displayValue += num;
+	}
+
+	displayUpdate();
 }
 
 displayUpdate();
